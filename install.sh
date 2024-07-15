7f1fdc468e001358ceb2def8114cbfc5b796a244
ChenYing Kuo <evshary@gmail.com>
2024-07-15 11:43:03 +0800

    Reorganize the install.sh
    Signed-off-by: ChenYing Kuo <evshary@gmail.com>

--- install.sh
+++ install.sh
@@ -7,7 +7,9 @@ sudo apt install tmux vim vim-gtk zsh \
                  git exuberant-ctags ccls \
                  silversearcher-ag ripgrep \
                  xclip curl python3-pip python3-argcomplete
-# install bat
+
+
+# bat: print with color
 # We need to install this way since the confliction of bat and ripgrep
 # https://github.com/BurntSushi/ripgrep/issues/1562
 apt-get download bat
@@ -16,18 +18,29 @@ rm bat*.deb
 mkdir -p ~/.local/bin
 ln -s /usr/bin/batcat ~/.local/bin/bat
 
-# install RUST
+
+# RUST environment setup
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 source "$HOME/.cargo/env"
 
-# install python poetry
+
+# Python environment setup
+## poetry
 curl -sSL https://install.python-poetry.org | python3 -
+### poetry: Always make sure the virtualenv is under the project
 poetry config virtualenvs.in-project true
+## Ruff: Python3 linter & fixer
+python3 -m pip install ruff
+## pre-commit: Add hooks to git commit
+### Run `pre-commit install --install-hooks` to enable `.pre-commit-config.yaml`
+python3 -m pip install pre-commit
+
 
 # install nvm (This is necessary for vim Coc plugin)
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 source "$HOME/.nvm/nvm.sh"
 nvm install node
+
 
 # vim settings
 echo "vim settings ..."
@@ -36,6 +49,7 @@ rm -rf ~/.vim
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 ln -s ~/dotfiles/vimrc ~/.vimrc
 ln -s ~/dotfiles/coc-settings.json ~/.vim/coc-settings.json
+
 
 # zsh settings
 echo "zsh settings ..."
@@ -49,11 +63,13 @@ rm ~/.zshrc
 ln -s dotfiles/zshrc ~/.zshrc
 rm -rf ~/.zsh
 
-# fzf installation
+
+# fzf: fuzzy finder
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 
-# Install ranger
+
+# ranger: file manager in console
 python3 -m pip install ranger-fm
 rm -rf ~/.config/ranger/*
 ranger --copy-config=all
@@ -61,17 +77,14 @@ ranger --copy-config=all
 cat ranger/commands.py >> ~/.config/ranger/commands.py
 cat ranger/rc.conf >> ~/.config/ranger/rc.conf
 
-# Install ruff (Python3 linter & fixer)
-python3 -m pip install ruff
 
-# Install pre-commit
-python3 -m pip install pre-commit
-
-# Install flatpak
+# flatpak: application management tool
 sudo apt install flatpak
 
-# Install SDKMAN
+
+# SDKMAN: sdk management tool
 curl -s "https://get.sdkman.io" | bash
+
 
 # Other settings
 echo "other settings ..."
@@ -81,3 +94,4 @@ rm -rf ~/.tmux.conf
 ln -s dotfiles/tmux.conf ~/.tmux.conf
 rm -rf ~/.gitconfig
 ln -s dotfiles/gitconfig ~/.gitconfig
+
