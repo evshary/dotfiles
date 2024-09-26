da79bda844934bb9ddbac2d1e429ece7816094be
ChenYing Kuo <evshary@gmail.com>
2024-09-26 11:16:16 +0800

    Support to install with Ubuntu 24.04
    Signed-off-by: ChenYing Kuo <evshary@gmail.com>

---
+++ ubuntu2404/install.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+
+# Stop whenever any commit fails
+set -e
+
+echo "======== Start to install basic dotfiles configuration ========"
+echo ""
+
+
+# install packages
+sudo apt update
+sudo apt install tmux vim zsh git bat curl wget ssh just \
+                 exuberant-ctags ccls \
+                 silversearcher-ag ripgrep \
+                 python3-pip pipx
+
+
+# RUST environment setup
+if [ ! -d ~/.cargo ]; then
+  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+  source "$HOME/.cargo/env"
+fi
+
+
+# Python environment setup
+## poetry
+curl -sSL https://install.python-poetry.org | python3 -
+### poetry: Always make sure the virtualenv is under the project
+poetry config virtualenvs.in-project true
+## pyenv
+if [ ! -d ~/.pyenv ]; then
+    curl https://pyenv.run | bash
+fi
+## Ruff: Python3 linter & fixer
+pipx install ruff
+## pre-commit: Add hooks to git commit
+### Run `pre-commit install --install-hooks` to enable `.pre-commit-config.yaml`
+pipx install pre-commit
+
+
+# install nvm (This is necessary for vim Coc plugin)
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+source "$HOME/.nvm/nvm.sh"
+nvm install node
+
+
+# vim settings
+rm ~/.vimrc
+ln -s ~/dotfiles/vimrc ~/.vimrc
+if [ ! -d ~/.vim ]; then
+  curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+  ln -s ~/dotfiles/coc-settings.json ~/.vim/coc-settings.json
+fi
+
+
+# zsh settings
+if [ ! -d ~/.oh-my-zsh ]; then
+  curl -L http://install.ohmyz.sh | sh
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+  git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+  git clone https://github.com/paulirish/git-open.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/git-open
+fi
+rm ~/.zshrc
+ln -s dotfiles/zshrc ~/.zshrc
+rm -rf ~/.zsh
+
+
+# fzf: fuzzy finder
+if [ ! -d ~/.fzf ]; then
+  git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+  ~/.fzf/install
+fi
+
+
+# ranger: file manager in console
+pipx install ranger-fm
+rm -rf ~/.config/ranger/*
+ranger --copy-config=all
+# Settings is from https://yangxuanx.github.io/2019/03/11/ranger%E7%9A%84%E4%BD%BF%E7%94%A8/
+cat ranger/commands.py >> ~/.config/ranger/commands.py
+cat ranger/rc.conf >> ~/.config/ranger/rc.conf
+
+
+# Other settings
+rm -rf ~/.tmux.conf
+ln -s dotfiles/tmux.conf ~/.tmux.conf
+rm -rf ~/.gitconfig
+ln -s dotfiles/gitconfig ~/.gitconfig
+
+
+echo ""
+echo "======== Basic dotfiles installation is done ======="
+
---
+++ ubuntu2404/install_full.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Stop whenever any commit fails
+set -e
+
+echo "======== Start to install full dotfiles configuration ========"
+echo ""
+
+# Install the development environment
+./ubuntu2404/install.sh
+
+
+# flatpak: application management tool
+sudo apt install -y flatpak
+
+
+# SDKMAN: sdk management tool
+curl -s "https://get.sdkman.io" | bash
+
+
+# docker without sudo privilege
+sudo apt install -y docker.io
+sudo groupadd docker
+sudo usermod -aG docker $USER
+
+echo ""
+echo "======== Full dotfiles installation is done ======="
+
