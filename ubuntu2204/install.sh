8812cf086e6e5d5cfb7197f0d45d582a81489040
ChenYing Kuo <evshary@gmail.com>
2024-09-26 14:14:00 +0800

    Another way to install just.
    Signed-off-by: ChenYing Kuo <evshary@gmail.com>

--- ubuntu2204/install.sh
+++ ubuntu2204/install.sh
@@ -103,7 +103,10 @@ cat ranger/rc.conf >> ~/.config/ranger/rc.conf
 
 
 # just
-pipx install just
+wget -qO - 'https://proget.makedeb.org/debian-feeds/prebuilt-mpr.pub' | gpg --dearmor | sudo tee /usr/share/keyrings/prebuilt-mpr-archive-keyring.gpg 1> /dev/null
+echo "deb [arch=all,$(dpkg --print-architecture) signed-by=/usr/share/keyrings/prebuilt-mpr-archive-keyring.gpg] https://proget.makedeb.org prebuilt-mpr $(lsb_release -cs)" | sudo tee /etc/apt/sources.list.d/prebuilt-mpr.list
+sudo apt update
+sudo apt install just
 
 
 # Other settings
