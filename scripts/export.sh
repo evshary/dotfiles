a01adfad0bcbb3a81702f516969f0f943969da27
ChenYing Kuo <evshary@gmail.com>
2025-10-06 16:46:19 +0800

    Fix GPG key update
    Signed-off-by: ChenYing Kuo <evshary@gmail.com>

--- scripts/export.sh
+++ scripts/export.sh
@@ -2,7 +2,7 @@
 
 # Refer to https://medium.com/@mattdark/backup-restore-github-gpg-ssh-keys-a335db22b953sudo%20apt%20install%20brave-browserk
 mkdir backup
-cp ~/.ssh/id_rsa* backup
+#cp ~/.ssh/id_* backup
 gpg_id=`gpg --list-secret-keys --keyid-format LONG | grep sec | awk -F/ '{print $2}' | awk '{print $1}'`
 gpg --export-secret-keys $gpg_id > backup/my-private-key.asc
 
--- scripts/import.sh
+++ scripts/import.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Refer to https://medium.com/@mattdark/backup-restore-github-gpg-ssh-keys-a335db22b953sudo%20apt%20install%20brave-browserk
-mv backup/id_rsa* ~/.ssh/
-chmod 600 ~/.ssh/id_rsa
-chmod 644 ~/.ssh/id_rsa.pub
-ssh-add ~/.ssh/id_rsa
+#mv backup/id_rsa* ~/.ssh/
+#chmod 600 ~/.ssh/id_ed25519
+#chmod 644 ~/.ssh/id_ed25519.pub
+#ssh-add ~/.ssh/id_ed25519
 gpg --import backup/my-private-key.asc
 
